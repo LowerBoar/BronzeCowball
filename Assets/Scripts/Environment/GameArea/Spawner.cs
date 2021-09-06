a72d0e3e2883be30f86027b332a8fb6bfa4ae24d
Egor Kutselabskii <osutanfirst@gmail.com>
2021-09-06 22:04:17 +1000

    Attempt to make split-floor spawning system

--- Assets/Scripts/Environment/GameArea/Spawner.cs
+++ Assets/Scripts/Environment/GameArea/Spawner.cs
@@ -7,23 +7,32 @@ using Random = UnityEngine.Random;
 public class Spawner : MonoBehaviour
 {
 	public GameObject FloorCell;
+	private int Partition = 40;
 
 	private GameObject spawnPoint;
 	private GameObject holder;
-	private Camera mainCamera;
+	private float sizeX;
 
 	void Start()
 	{
 		var holder = transform.Find("Cells");
-		mainCamera = GetComponentInParent<Camera>();
+		sizeX = GetComponentInParent<Camera>().orthographicSize * 2;
 		var spawnPointSprite = GetComponentInChildren<SpriteRenderer>();
 
 		spawnPoint = spawnPointSprite.gameObject;
 		spawnPointSprite.enabled = false;
 
+		var point = Random.Range(2, Partition - 2);
+		var cellSize = sizeX / Partition;
+
 		var cell = Instantiate(FloorCell, holder);
-		cell.transform.position = spawnPoint.transform.position;
-		cell.transform.localScale = new Vector3(mainCamera.orthographicSize * 2, 1f);
+		cell.transform.position =
+			spawnPoint.transform.position - new Vector3((Partition / 2 - point - 1) * cellSize, 0);
+		cell.transform.localScale = new Vector3(sizeX * point, 1f);	// TODO Scale won't do. Need to think of some other size-control system
+
+		cell = Instantiate(FloorCell, holder);
+		cell.transform.position = spawnPoint.transform.position - new Vector3((Partition / 2 - point + 1) * cellSize, 0);
+		cell.transform.localScale = new Vector3(sizeX * (Partition - point), 1f);
 	}
 
     void Update()
