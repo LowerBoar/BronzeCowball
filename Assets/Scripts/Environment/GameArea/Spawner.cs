5bde6e9d4524fdd0d80d9a438755a15aa3e04d5a
Egor Kutselabskii <osutanfirst@gmail.com>
2021-09-15 22:26:34 +1000

    Add spawning with 3-second cooldown

--- Assets/Scripts/Environment/GameArea/Spawner.cs
+++ Assets/Scripts/Environment/GameArea/Spawner.cs
@@ -10,35 +10,40 @@ public class Spawner : MonoBehaviour
 	private int Partition = 10;
 
 	private GameObject spawnPoint;
-	private GameObject holder;
+	private Transform holder;
 	private float sizeX;
 
 	void Start()
 	{
-		var holder = transform.Find("Cells");
+		holder = transform.Find("Cells");
 		sizeX = GetComponentInParent<Camera>().orthographicSize * 2;
 		var spawnPointSprite = GetComponentInChildren<SpriteRenderer>();
 
 		spawnPoint = spawnPointSprite.gameObject;
 		spawnPointSprite.enabled = false;
 
-		var point = Random.Range(2, Partition - 2);
-		var cellSize = sizeX / Partition;
-
-		var additionalOffset = 2;	// TODO Additional offset should be based on player size
-
-		var cell = Instantiate(FloorCell, holder);
-		cell.transform.position =
-			spawnPoint.transform.position - new Vector3(((float)point / 2 + additionalOffset) * cellSize, 0);
-		cell.transform.localScale = new Vector3(cellSize * point, 1f);
-
-		cell = Instantiate(FloorCell, holder);
-		cell.transform.position = spawnPoint.transform.position + new Vector3((float)(Partition - point) / 2 * cellSize, 0);
-		cell.transform.localScale = new Vector3(cellSize * (Partition - point), 1f);
+		InvokeRepeating(nameof(Spawn), 0f, 3f);
 	}
 
     void Update()
     {
         
     }
+
+    private void Spawn()
+    {
+	    var point = Random.Range(2, Partition - 2);
+	    var cellSize = sizeX / Partition;
+
+	    var additionalOffset = 2;   // TODO Additional offset should be based on player size
+
+	    var cell = Instantiate(FloorCell, holder);
+	    cell.transform.position =
+		    spawnPoint.transform.position - new Vector3(((float)point / 2 + additionalOffset) * cellSize, 0);
+	    cell.transform.localScale = new Vector3(cellSize * point, 1f);
+
+	    cell = Instantiate(FloorCell, holder);
+	    cell.transform.position = spawnPoint.transform.position + new Vector3((float)(Partition - point) / 2 * cellSize, 0);
+	    cell.transform.localScale = new Vector3(cellSize * (Partition - point), 1f);
+	}
 }
