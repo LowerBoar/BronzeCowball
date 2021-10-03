304b6aba7f13cc1e487e5183eaab9f76c8e83ae0
Egor Kutselabskii <osutanfirst@gmail.com>
2021-10-03 19:01:22 +1000

    Add sound to play button click

--- Assets/Scripts/MainMenu/MainMenuUIController.cs
+++ Assets/Scripts/MainMenu/MainMenuUIController.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 
 public class MainMenuUIController : MonoBehaviour
 {
+	public AudioClip PlayButtonClickSound;
+
 	void Start()
     {
         GetComponentInChildren<Button>().onClick.AddListener(OnPlayButtonClick);
     }
 
 	private void OnPlayButtonClick()
-    {
+	{
+		var audioSource = GetComponent<AudioSource>();	// TODO Move this to SoundManager
+		audioSource.clip = PlayButtonClickSound;
+		audioSource.Play();
+
 	    FindObjectOfType<GameManager>().BeginPlaying();
 	    SceneManager.LoadScene(Globals.GetSceneIndex(Globals.Scenes.GameScreen));
     }
