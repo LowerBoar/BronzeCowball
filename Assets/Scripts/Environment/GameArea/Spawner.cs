cf983bfd70ffb9fc99a49926a976fd7ab63bd253
Egor Kutselabskii <osutanfirst@gmail.com>
2021-10-13 16:23:39 +1000

    Refactor spawner

--- Assets/Scripts/Environment/GameArea/Spawner.cs
+++ Assets/Scripts/Environment/GameArea/Spawner.cs
@@ -12,11 +12,16 @@ public class Spawner : MonoBehaviour
 
 	private GameObject spawnPoint;
 	private float sizeX;
+	private float cellSize;
+	private float holeSize;
 	private float spawnDelay = 3f;
 
 	void Start()
 	{
 		sizeX = ReferenceFloor.localScale.x;
+		cellSize = sizeX / Partition;
+		holeSize = 1.5f * FindObjectOfType<Player>().transform.localScale.x * cellSize;
+
 		var spawnPointSprite = GetComponentInChildren<SpriteRenderer>();
 
 		spawnPoint = spawnPointSprite.gameObject;
@@ -37,24 +42,20 @@ public class Spawner : MonoBehaviour
 
     private void Spawn()
     {
+	    var positionOffset = spawnPoint.transform.position;
 	    var point = Random.Range(2, Partition - 2);
-	    var cellSize = sizeX / Partition;
 
-	    var additionalOffset = 1.5f * FindObjectOfType<Player>().transform.localScale.x;
+		CreateCell(cellSize * point - holeSize / 2, positionOffset);
+		positionOffset.x += cellSize * point + holeSize / 2;
+		
+		CreateCell(cellSize * (Partition - point) - holeSize / 2, positionOffset);
+    }
 
+    private void CreateCell(float xSize, Vector3 positionOffset)
+    {
 	    var cell = Instantiate(FloorCell, cellHolder);
 	    cell.transform.parent = cellHolder;
-	    var size = new Vector3(cellSize * point - additionalOffset * cellSize / 2, 1f);
-	    cell.transform.localScale = size;
-	    cell.transform.position =
-		    spawnPoint.transform.position + new Vector3(size.x / 2, 0);
-
-	    var sizeSum = size.x;
-
-	    cell = Instantiate(FloorCell, cellHolder);
-	    cell.transform.parent = cellHolder;
-		size = new Vector3(cellSize * (Partition - point) - additionalOffset * cellSize / 2, 1f);
-		cell.transform.localScale = size;
-		cell.transform.position = spawnPoint.transform.position + new Vector3(size.x / 2 + sizeSum + additionalOffset * cellSize, 0);	// TODO Replace this ugly code with something clean
+	    cell.transform.localScale = new Vector3(xSize, 1f);
+	    cell.transform.position = positionOffset + new Vector3(xSize / 2, 0);
     }
 }
