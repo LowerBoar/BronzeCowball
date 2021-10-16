dcadce45c325df7e56c368b67ad1343d0d6e8f60
Egor Kutselabskii <osutanfirst@gmail.com>
2021-10-16 17:11:20 +1000

    Simplify spawner code, fix missing CellHolder prefab

--- Assets/Scripts/Environment/GameArea/Spawner.cs
+++ Assets/Scripts/Environment/GameArea/Spawner.cs
@@ -45,18 +45,14 @@ public class Spawner : MonoBehaviour
 	    var holesAmount = Random.Range(1, 4);
 
 	    var positionOffset = spawnPoint.transform.position;
+	    var lastPoint = 0;
 
-	    var point = Random.Range(1, Partition - 1);
-	    CreateCell(cellSize * point - halfHoleSize, positionOffset);
-	    positionOffset.x += cellSize * point + halfHoleSize;
-	    var lastPoint = point;
-
-	    for (var i = 0; i < holesAmount - 1; ++i) {
+	    for (var i = 0; i < holesAmount; ++i) {
 		    if (lastPoint + 1 >= Partition - 1) {
 			    break;
 		    }
 
-		    point = Random.Range(lastPoint + 1, Partition - 1);
+		    var point = Random.Range(lastPoint + 1, Partition - 1);
 		    var distance = point - lastPoint;
 		    CreateCell(cellSize * distance - halfHoleSize, positionOffset);
 		    positionOffset.x += cellSize * distance + halfHoleSize;
